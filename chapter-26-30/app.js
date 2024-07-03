54dd6f58468a51015ad70f8fb94d412faab986ee
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-07-03 06:43:18 +0500

    add q 21 to 30

---
+++ chapter-26-30/app.js
@@ -0,0 +1,102 @@
+// Q no 1
+
+
+// let userInput = prompt("Enter a numbers")
+//  let num = 3.4512;
+// let roundedValue = Math.round(num)
+
+// // // a. number
+// document.write(`number: ${num} <br/>`)
+
+// // // b. round off value of the number
+// document.write(`round off value: ${roundedValue}<br/>`)
+
+
+// // // c. floor value of the number
+// let floorValue = Math.floor(num)
+// document.write(`floor value: ${floorValue} <br/>`)
+
+// // // d. ceil value of the number
+// let ceilValue = Math.ceil(num)
+// document.write(`ceil value: ${ceilValue}`)
+
+
+// // Q no 2
+
+// let userInput = prompt("Enter a numbers which is starting from -sign ")
+//  let num = -2.673;
+// let roundedValue = Math.round(num)
+
+// // // a. number
+// document.write(`number: ${num} <br/>`)
+
+// // // b. round off value of the number
+// document.write(`round off value: ${roundedValue}<br/>`)
+
+
+// // // c. floor value of the number
+// let floorValue = Math.floor(num)
+// document.write(`floor value: ${floorValue} <br/>`)
+
+// // // d. ceil value of the number
+// let ceilValue = Math.ceil(num)
+// document.write(`ceil value: ${ceilValue}`)
+
+
+
+// Q no 3
+
+// let num = +prompt("Enter any number to get absolute value");
+// document.write(Math.abs(num))
+
+
+// Q no 4
+
+
+// // let randomDiceValue = Math.random()*4;
+
+// let randomDiceValue1 = Math.random()*6;
+// document.write(`random dice value: ${randomDiceValue}`);
+// document.write(`random dice value: ${randomDiceValue1}`);
+
+
+
+// Q no 5
+
+
+// let game = ["head", "tail"];
+// let gen = Math.random() * game.length;
+// let convertToFloor = Math.floor(gen);
+// document.write(`${convertToFloor}<br>`);
+// let gettingGame = game[convertToFloor];
+// document.write(`Random Coin Value : ${gettingGame}`);
+
+
+// Q no 6
+
+
+// let randomNumber =(Math.ceil( Math.random() * 100))
+// document.write(`random number between 1 and 100: ${randomNumber}`)
+
+
+
+// Q no 7
+
+
+// let userInput = prompt("Enter your weight in kgs")
+
+// document.write(`The weight of user is ${userInput} kilograms`)
+
+// Q no 8
+
+
+
+// let UserInput = +prompt("Enter a number between 1 to 10");
+
+// let myNum = 7;
+//  if (UserInput == myNum){
+// alert("Congratulation")
+//  }else {
+//     alert("Sorry")
+//  }
+
